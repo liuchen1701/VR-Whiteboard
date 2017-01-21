0986996b7dd18ae0a47c560d8627bfccd2340439
gajeelSF <yudakid@gmail.com>
2017-01-20 20:56:08 -0800

    first push

---
+++ Assets/Scripts/draw.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class draw : MonoBehaviour {
+    public LineRenderer laser;
+    public GameObject painter;
+
+    void Start()
+    {
+        Vector3[] initLaserPositions = new Vector3[2] { Vector3.zero, Vector3.zero };
+        laser.SetPositions(initLaserPositions);
+        laser.SetWidth(0.01f, 0.01f);
+    }
+
+    void Update()
+    {
+        Vector3 v = GvrController.Orientation * Vector3.forward;
+
+        ShootLaserFromTargetPosition(transform.position, v, 200f);
+        laser.enabled = true;
+    }
+
+    void ShootLaserFromTargetPosition(Vector3 targetPosition, Vector3 direction, float length)
+    {
+        Ray ray = new Ray(targetPosition, direction);
+        RaycastHit raycastHit;
+        if( Physics.Raycast(ray, out raycastHit, length))
+        {
+            Vector3 position = raycastHit.point;
+            print(position.ToString());
+            if(GvrController.IsTouching)
+            {
+                Instantiate(painter, position, Quaternion.identity);
+            }
+        }
+
+        Vector3 endPosition = targetPosition + (length * direction);
+        laser.SetPosition(0, targetPosition);
+        laser.SetPosition(1, endPosition);
+    }
+}
