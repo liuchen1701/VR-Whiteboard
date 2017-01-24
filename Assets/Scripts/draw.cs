6e507bee5c95fa0fdd7855800d7f0493cbd2f20e
Luis Calona <lcalona@ucsd.edu>
2017-01-23 16:50:47 -0800

    First Push

--- Assets/Scripts/draw.cs
+++ Assets/Scripts/draw.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class draw : MonoBehaviour {
-    public LineRenderer laser;
-    public GameObject painter;
+
+    public Material material;
 
     void Start()
     {
@@ -11,20 +11,32 @@ public class draw : MonoBehaviour {
 
     void Update()
     {
+        Texture2D texture = (Texture2D)material.GetTexture("white");
+
         Vector3 direction = GvrController.Orientation * Vector3.forward;
 
         Ray ray = new Ray(transform.position, direction);
         RaycastHit raycastHit;
 
-        if (Physics.Raycast(ray, out raycastHit, 200f) && raycastHit.transform.gameObject.name.Equals("WhiteBoard"))
+        if (Physics.Raycast(ray, out raycastHit, 200f) && raycastHit.transform.gameObject.name.Equals("Whiteboard"))
         {
-            Vector3 position = raycastHit.point;
-            position = new Vector3(position.x, position.y, position.z - 0.5f);
-
+            Vector3 hitPosition = raycastHit.point;
+            int x = (int)hitPosition.x;
+            int y = (int)hitPosition.y;
+            
             if (GvrController.IsTouching)
             {
-                Instantiate(painter, position, painter.transform.rotation);
-                print(position.ToString());
+                for(int i = -10; i <= 10; i++)
+                {
+                    for(int j = -10; j <= 10; j++)
+                    {
+                        texture.SetPixel(x + i, y + j, Color.black);
+                    }
+                    
+                }
+                
+                texture.Apply();
+                material.SetTexture("drawing", texture);
             }
         }
     }
