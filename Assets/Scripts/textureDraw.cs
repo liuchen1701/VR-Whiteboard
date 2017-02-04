8109ffadc57fe4a60d44942fc7b0fa9951fb9aad
Luis Calona <lcalona@ucsd.edu>
2017-02-03 20:13:38 -0800

    Worked on texture painting with controller
    Still need to fix accuracy

---
+++ Assets/Scripts/textureDraw.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textureDraw : MonoBehaviour {
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {        
+        Texture2D texture = Texture2D.whiteTexture;
+
+        //Texture2D texture = new Texture2D(1, 1);
+
+        GetComponent<Renderer>().material.mainTexture = texture;
+
+        //Vector3 pointerPos = GvrController.Orientation * Vector3.forward;
+
+        Vector3 pointerPos = GvrController.ArmModel.pointerPosition;
+
+        if (GvrController.IsTouching)
+        {
+            int x = (int)pointerPos.x;
+            int y = (int)pointerPos.y;
+
+            Debug.Log("x: " + x + " y: " + y);
+
+            texture.SetPixel(x, y, Color.black);
+            texture.Apply();
+        }
+
+        // Clear board
+        if (GvrController.AppButton)
+        {
+            for (int y = 0; y < texture.height; y++)
+            {
+                for (int x = 0; x < texture.width; x++)
+                {
+                    Color color = Color.white;
+                    texture.SetPixel(x, y, color);
+                }
+            }
+            texture.Apply();
+        }
+    }
+
+    /* Mouse Draw
+	void Update ()
+    {
+        Texture2D texture = Texture2D.whiteTexture;
+
+        GetComponent<Renderer>().material.mainTexture = texture;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            int x = (int)mousePos.x;
+            int y = (int)mousePos.y;
+
+            texture.SetPixel(x, y, Color.black);
+            texture.Apply();
+        }
+
+        if ( Input.GetMouseButtonDown(1) )
+        {
+            for (int y = 0; y < texture.height; y++)
+            {
+                for (int x = 0; x < texture.width; x++)
+                {
+                    Color color = Color.white;
+                    texture.SetPixel(x, y, color);
+                }
+            }
+            texture.Apply();
+            Debug.Log("Mouse Clicked");
+        }
+    }
+    */
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# VR-Whiteboard
-A Whiteboard prototype in Daydream VR Platform
